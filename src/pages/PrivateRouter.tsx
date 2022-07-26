545bc06c85521f2e0ace0853bc9cab64c1fb0ee4
Hai <checked.kenvip10@gmail.com>
2022-07-26 18:22:24 +0700

    check

--- src/pages/PrivateRouter.tsx
+++ src/pages/PrivateRouter.tsx
@@ -1,16 +1,27 @@
-import React, {Children} from 'react'
+import axios from 'axios';
+import React, {Children, useEffect, useState} from 'react'
 import {Navigate} from 'react-router-dom'
 type PrivateRouterProps = {
     children: JSX.Element;
 }
 
 const PrivateRouter = (props: PrivateRouterProps) => {
+    const [key, setKey] = useState<any[]>([])
+    useEffect(() => {
+        console.log("fuckbitch");
+        const getKey =  async () => {
+                const {data} = await axios.get("https://bufflikea.herokuapp.com/keys");
+                setKey(data)
+        }
+        getKey
+    },[])
     const keys = JSON.parse(localStorage.getItem("keys") as string).name
-    console.log(keys);
-    if(keys) {
+    const check = key.filter((item) => item.name == keys);
+    
+    if(check) {
         return props.children
     }else {
-        return <Navigate to="/key" />
+        return <Navigate to="https://tanglike.app/key" />
     }
  
 }
